ca312aa8e57ff380e4b54ae18586be8b74ca8175
sprThiru <87995087+sprThiru@users.noreply.github.com>
2021-09-08 10:43:45 +0530

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,57 @@
+img = "";
+statuss = "";
+objects = [];
+function preload()
+{
+    img = loadImage("bottle.jpg");
+}
+
+function setup()
+{
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : object detected";
+}
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    statuss=true;
+    objectDetector.detect(img,gotResult);
+}
+function back()
+{
+    window.location="index.html";  
+}
+function draw()
+{
+    image(img,0,0,640,420);
+
+    if(statuss != "")
+    {
+
+    for(i = 0; i < objects.length; i++)
+    {
+        document.getElementById("status").innerHTML="Status : object dected";
+        fill("#FF0000");
+
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label + " " + percent + "%" , objects[i].x,objects[i].y);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+    }
+
+}
+
+}
+function gotResult(error,results)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+        console.log(results);
+        objects = results;
+}
